1784de7cccf7e7640f7793b7b879d9cfae91e708
BeginnerJay <37648120+BeginnerJay@users.noreply.github.com>
2021-11-06 22:45:55 +0900

    made express server

--- vue-project/vue.config.js
+++ vue-project/vue.config.js
@@ -1,5 +1,17 @@
 module.exports = {
   chainWebpack: config => {
     config.plugins.delete('prefetch') //prefetch 기능 disable
-  }
+  },
+  devServer: {
+    proxy: {
+      '/api' : {
+        target: 'http://localhost:3000/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  },
+  outputDir: '../backend/public'
 }
